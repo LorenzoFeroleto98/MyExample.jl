32c31870d727c7a08f82d0b5f7e7daaf8fe3ec1a
Lorenzo <lorenzo.feroleto.dev@gmail.com>
2020-03-11 18:52:22 +0100

    add the my_f function
    my_f is a linear function just for demonstration

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,5 +1,11 @@
 module MyExample
 
 greet() = print("Hello World!")
+include("extra_file.jl")
+
+#= You should export only functions for public use.
+	Internal function should NEVER be export but imported (import, not using)
+=#
+export my_f
 
 end # module
