ba68330fbce82d8728ed990f22874a66bf9f1b62
Lorenzo <lorenzo.feroleto.dev@gmail.com>
2020-03-12 14:39:08 +0100

    Add ForwardDiff dependency and setup SafeTestsets

--- src/MyExample.jl
+++ src/MyExample.jl
@@ -1,11 +1,13 @@
 module MyExample
 
+using ForwardDiff
+
 greet() = print("Hello World!")
 include("extra_file.jl")
 
 #= You should export only functions for public use.
 	Internal function should NEVER be export but imported (import, not using)
 =#
-export my_f
+export my_f, derivative_of_my_f
 
 end # module
